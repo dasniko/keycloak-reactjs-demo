dd0920897565fbe054479a11e1b3b1714d942a57
Niko Köbler <niko@n-k.de>
2020-02-18 20:45:13 +0100

    add redux logger enhancer
    for development environment

--- src/services/StoreService.js
+++ src/services/StoreService.js
@@ -1,23 +1,29 @@
 import thunk from "redux-thunk";
 import {createBrowserHistory} from "history";
 import {routerMiddleware} from "react-router-redux";
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import axiosMiddleware from "redux-axios-middleware";
+import logger from "redux-logger";
 import rootReducer from "../modules";
 import HttpService from "./HttpService";
 
 const browserHistory = createBrowserHistory();
 
 const setup = () => {
+  const enhancers = [];
   const middleware = [
     thunk,
     routerMiddleware(browserHistory),
     axiosMiddleware(HttpService.getAxiosClient())
   ];
-  return createStore(
-    rootReducer,
-    applyMiddleware(...middleware)
-  );
+
+  if (process.env.NODE_ENV === 'development') {
+    enhancers.push(applyMiddleware(logger));
+  }
+
+  const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+
+  return createStore(rootReducer, composedEnhancers);
 };
 
 export default {
