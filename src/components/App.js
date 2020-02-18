6b3ce20792895c8c8721ebd1f204b3a455454e5d
Niko Köbler <niko@n-k.de>
2020-02-18 20:04:19 +0100

    cleanup the index and create an App component

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {Provider} from "react-redux";
+import {Router, Route, Switch} from "react-router-dom";
+import BookBox from "../components/BookBox";
+import BookDetails from "../components/BookDetails";
+
+const App = ({ store, history }) => (
+  <Provider store={store}>
+    <Router history={history}>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={BookBox}/>
+          <Route path="/books/:bookId" component={BookDetails}/>
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+);
+
+App.propTypes = {
+  history: PropTypes.any.isRequired,
+  store: PropTypes.any.isRequired
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {Router, Route, Switch} from "react-router-dom";
-import BookBox from "./components/BookBox";
-import BookDetails from "./components/BookDetails";
+import App from "./components/App";
 import HttpService from "./services/HttpService";
 import StoreService from "./services/StoreService";
 import UserService from "./services/UserService";
@@ -11,20 +8,7 @@ import UserService from "./services/UserService";
 const store = StoreService.setup();
 const history = StoreService.browserHistory;
 
-const app = (
-  <Provider store={store}>
-    <Router history={history}>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={BookBox}/>
-          <Route path="/books/:bookId" component={BookDetails}/>
-        </Switch>
-      </div>
-    </Router>
-  </Provider>
-);
-
-const renderApp = () => ReactDOM.render(app, document.getElementById("app"));
+const renderApp = () => ReactDOM.render(<App {...{ store, history }} />, document.getElementById("app"));
 
 UserService.initKeycloak(renderApp);
 HttpService.configure();
