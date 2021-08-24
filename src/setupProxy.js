3cb576044dbee80014ef8c487087f5ccda1b698f
Niko Köbler <niko@n-k.de>
2021-08-24 16:10:28 +0200

    upgrade proxy middleware, resolves #28

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/demo', {
+  app.use('/demo', createProxyMiddleware({
     target: 'https://erah07zkak.execute-api.eu-central-1.amazonaws.com',
     changeOrigin: true
-  }))
+  }));
 };
